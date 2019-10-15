4db66755d647dadd5753cd24b78fef6420e8cc7e
liuzy <liuzy@liuzy88.com>
2019-10-15 23:56:57 +0800

    fix

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.get('/', (req, res, next) => {
             const data = yield DB.Model.findOne({where: {id: id}});
             if (Conf.www.mode === 'offline') {
                 const flag = yield Comm.openVideo(Conf.exp, data);
-                if (flag)
-                    res.end('Play failure.')
-                else
-                    res.end('<script>window.close()</script>')
+                res.end(flag ? 'Play failure.' : '<script>window.close()</script>')
             } else {
                 res.render('index', {data: data});
             }
